d82d5b99fcc3e6cbc2d29055b88c8a77a77990a8
Patrick Aldis <patricktaldis@gmail.com>
2026-08-22 23:10:58 +0100

    installer attribute

--- flake.nix
+++ flake.nix
@@ -53,10 +53,42 @@
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = [ 
           inputs.agenix.packages.${system}.agenix
-          pkgs.age
-          pkgs.xorriso
         ];
       };
-      nixosConfigurations = (genSystemsWith mkSystem) // { installers = genSystemsWith mkInstaller; } ;
+      packages.${system}.mkInstallerFor = genSystemsWith (name: pkgs.writeShellApplication {
+        name = "Generate an installable ISO";
+        runtimeInputs = with pkgs; [age xorriso];
+        text = ''
+          # Decrypt keys to temp files
+          TMPDIR=$(mktemp -d)
+          trap 'rm -rf $TMPDIR' EXIT
+
+          echo "Decrypting keys..."
+          age -d -i ~/.ssh/id_ed25519 secrets/keys/prv/lydia.age > "$TMPDIR/lydia"
+          age -d -i ~/.ssh/id_ed25519 secrets/keys/prv/root_lydia.age > "$TMPDIR/root_lydia"
+          cp secrets/keys/pub/lydia.pub "$TMPDIR/lydia.pub"
+          cp secrets/keys/pub/root_lydia.pub "$TMPDIR/root_lydia.pub"
+
+          # Copy ISO and make writable
+          ISO_SRC="$(ls ${(mkInstaller name).config.system.build.isoImage}/iso/*.iso)"
+          ISO_OUT="installer.iso"
+
+          echo "Adding keys to ISO..."
+          cp "$ISO_SRC" "$ISO_OUT"
+          chmod +w "$ISO_OUT"
+
+          # Add keys to ISO
+          xorriso -indev "$ISO_OUT" -outdev "$ISO_OUT" \
+            -map "$TMPDIR/lydia" /lydia \
+            -map "$TMPDIR/lydia.pub" /lydia.pub \
+            -map "$TMPDIR/root_lydia" /root_lydia \
+            -map "$TMPDIR/root_lydia.pub" /root_lydia.pub \
+            -boot_image any replay \
+            2>/dev/null
+
+          echo "Done: $ISO_OUT"
+        '';
+      });
+      nixosConfigurations = (genSystemsWith mkSystem) ;
     };
 }
