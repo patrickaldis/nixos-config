5d7d3aba5a1c5f87a50ea93a73963502313e3043
Patrick Aldis <patricktaldis@gmail.com>
2026-08-25 07:41:29 +0000

    Rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,21 +1,22 @@
 let
   # systems
-  tv-box = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN7XKO8WEAkt43cSbOzHtraamI+BuKQh+ZEMMTYbljM4 magic-box";
-  obsidian-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEK8e8MqqUI41g5ymoXJY+NaW7TiEfH/M8qUX3YdB6Pi obsidian-laptop@obsidian-laptop";
-  vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMTu27K9MREISA+/1CjATVrQTp3cFomIoV0G72nT2/3e root@nixos";
   lydia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDxTHQOpCFZWlEk7P7KVBFGxMiCUBT+6tDu/cjCCZ+jw lydia";
   elizabeth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN4Syg9D4bIk0BOqrb/23iQLXObFWNkGkH41vPG7WHsA elizabeth";
-  mary = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUgf+mHH85a2lwOpFmmFnK6jiy+Hzjl/LaGMxKc6zpG patrick@mary";
+  all_systems = [lydia elizabeth];
 
-  all = [tv-box obsidian-laptop vm lydia elizabeth mary];
+  # users
+  patrick_mary = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUgf+mHH85a2lwOpFmmFnK6jiy+Hzjl/LaGMxKc6zpG patrick@mary";
+  all_users = [patrick_mary];
+
+  all = all_users ++ all_systems;
 in
 {
-  all.publicKeys = all;
+  allUsers.publicKeys = all;
 
-  "keys/prv/lydia.age".publicKeys = all;
-  "keys/prv/root_lydia.age".publicKeys = all;
-  "keys/prv/elizabeth.age".publicKeys = all;
-  "keys/prv/patrick_elizabeth.age".publicKeys = all;
+  "keys/prv/lydia.age".publicKeys = all_users;
+  "keys/prv/root_lydia.age".publicKeys = all_users;
+  "keys/prv/elizabeth.age".publicKeys = all_users;
+  "keys/prv/patrick_elizabeth.age".publicKeys = all_users;
 
   "root-pass.age".publicKeys = all;
   "runner-token.age".publicKeys = all;
