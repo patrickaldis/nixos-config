0e500a4c8d39eddefba092fa18300d5901bc4434
Patrick Aldis <patrick.aldis@obsidian.systems>
2026-09-14 07:39:28 +0100

    rename to elizabeth

--- hosts/elizabeth/shared.nix
+++ hosts/elizabeth/shared.nix
@@ -1,6 +1,8 @@
 ({ pkgs, ... }: {
   nixpkgs.config.allowUnfree = true;
 
+  networking.hostName = "elizabeth";
+
   boot.loader = {
     efi.canTouchEfiVariables = true;
     systemd-boot.enable = true;
