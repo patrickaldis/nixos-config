6e42c79d17a48e5302c391d4bcc2663e61722d35
Patrick Aldis <patrick.aldis@obsidian.systems>
2026-09-19 13:15:10 +0100

    use name lydia

--- hosts/kitty/import-tree/sound/spotifyd.nix
+++ hosts/kitty/import-tree/sound/spotifyd.nix
@@ -1,9 +1,9 @@
-{
+{config, ...}:{
   systemd.services.spotifyd.serviceConfig.SupplementaryGroups = [ "audio" "pipewire" ];
   services.spotifyd = {
     enable = true;
     settings.global = {
-      device_name = "Magic Box";
+      device_name = config.networking.hostName;
       backend = "pulseaudio";
       zeroconf_port = 4070;
       use_mpris = false;
