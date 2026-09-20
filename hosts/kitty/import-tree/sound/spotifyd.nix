7b1ee26717c09e98ba67e2fb0a827ff3d62c92ad
Patrick Aldis <patrick.aldis@obsidian.systems>
2026-09-20 09:30:16 +0100

    pin to headphone jack

--- hosts/kitty/import-tree/sound/spotifyd.nix
+++ hosts/kitty/import-tree/sound/spotifyd.nix
@@ -5,6 +5,7 @@
     settings.global = {
       device_name = config.networking.hostName;
       backend = "pulseaudio";
+      device = "alsa_output.pci-0000_00_1f.3.analog-stereo"; # Headphone jack
       zeroconf_port = 4070;
       use_mpris = false;
     };
