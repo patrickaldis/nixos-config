b333897f739bf2ce22725d4e23306781632523d6
Patrick Aldis <patrick.aldis@obsidian.systems>
2026-09-22 00:13:23 +0100

    elizabeth age init

--- hosts/elizabeth/default.nix
+++ hosts/elizabeth/default.nix
@@ -5,7 +5,7 @@
     ./disko.nix
     ./users.nix
 
-    ({import-tree, home-manager, ...}:{
+    ({ import-tree, home-manager, ... }: {
       imports = [
         home-manager.nixosModules.default
       ];
@@ -13,5 +13,11 @@
       home-manager.useGlobalPkgs = true;
       home-manager.users.patrick = (import-tree ./home);
     })
+    {
+      age.init = keys: /* sh */ ''
+        install -D -m 600 ${keys}/elizabeth /etc/ssh/ssh_host_ed25519_key
+        install -D -m 644 ${keys}/elizabeth.pub /etc/ssh/ssh_host_ed25519_key.pub
+      '';
+    }
   ];
 }
--- hosts/elizabeth/users.nix
+++ hosts/elizabeth/users.nix
@@ -1,6 +1,5 @@
-{config, ...}:{
-  users.users.patrick = {
-    hashedPasswordFile = config.age.secrets.root-pass.path;
-  };
+{config, secrets, ...}:{
+  age.secrets.root-pass.file = "${secrets}/root-pass.age";
+  users.users.patrick.hashedPasswordFile = config.age.secrets.root-pass.path;
   users.mutableUsers = false;
 }
